afd54b698678c70ab0781c4272bbe5c11adeaec3
hchrist2010 <christhu@oregonstate.edu>
2019-07-10 15:59:08 -0700

    algorithm1

--- algorithm1.py
+++ algorithm1.py
@@ -1,26 +1,25 @@
-# PSEUDOCODE
-# MaxSub(a[1, … , n])
-# For each pair (i, j) with i <= j 
-#      compute a[i]+a[i+1]+· · · +a[j-1]+a[j]
-#      Keep the max sum found so far
-# Return max sum
 import random
 
-n = random.randint(2, 20)
+#arr = [22, -27, 38, -34, 49, 40, 13, -44, -13, 28, 46, 7, -26, 42, 29, 0, -6, 35, 23, -37, 10, 12, -2, 18, -12, -49, -10, 37, -5, 17, 6, -11, -22, -17, -50, -40, 44, 14, -41, 19, -15, 45, -23, 48, -1, -39, -46, 15, 3, -32, -29, -48, -19, 27, -33, -8, 11, 21, -43, 24, 5, 34, -36, -9, 16, -31, -7, -24, -47, -14, -16, -18, 39, -30, 33, -45, -38, 41, -3, 4, -25, 20, -35, 32, 26, 47, 2, -4, 8, 9, 31, -28, 36, 1, -21, 30, 43, 25, -20, -42]
+n = random.randint(2,20)
+arr = []
+sum = 0
+local_sum = 0
 
-running_sum = 0;
-max_sum = 0;
-array = []
-
+#n is a randomly generated int, this will populate an array of size n with random numbers
 for i in range(n):
-    array.append(random.randint(-100,100))
+    arr.append(random.randint(-100,100))
 
-for j in range(0, len(array)):
-    running_sum += array[j];
-    if running_sum < 0:
-        running_sum = 0;
-    if max_sum < running_sum:
-        max_sum = running_sum;
-print(max_sum);
-print(array)
-    
+for i in range(0,n-1):
+    for j in range(1,n+1):
+        for k in range(i,j):
+            local_sum += arr[k]
+
+        if(local_sum > sum):
+            sum = local_sum
+
+        local_sum = 0
+
+print(sum)
+print(arr)
+input()
